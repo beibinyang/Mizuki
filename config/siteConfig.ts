0c8959ee9c777bdb23f9cf4d0afc5ec9e5f99659
beibinyang <153931666+beibinyang@users.noreply.github.com>
2026-08-01 18:13:04 +0800

    Update siteConfig.ts

--- config/siteConfig.ts
+++ config/siteConfig.ts
@@ -7,7 +7,7 @@ export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
 	siteURL: "https://mizuku.cccccccc.qzz.io/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-08-31", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
 
